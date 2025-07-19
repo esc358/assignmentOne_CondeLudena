a865934dd146310f80c51fa9bd063f29ca5915f6
esc358 <200478144@student.georgianc.on.ca>
2025-07-19 19:18:45 -0400

    initial commit

---
+++ app/src/main/java/ca/georgiancollege/assignmentone_condeludena/MainActivity.java
@@ -0,0 +1,30 @@
+/*
+* Emilio Sebastian Conde Ludena
+* 200478144
+* July 19, 2025
+* 7:16pm
+* */
+package ca.georgiancollege.assignmentone_condeludena;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
